20b791500e93acaf9a627880bf552f06e67fb126
Fuzail Raashid <fuzail385@gmail.com>
2025-04-29 21:22:29 +0530

    routing issue resolved | Fuzail Raashid

--- my_dashboard/src/app/pages/layout/layout.component.ts
+++ my_dashboard/src/app/pages/layout/layout.component.ts
@@ -3,11 +3,11 @@ import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import {  MatSidenavModule } from '@angular/material/sidenav';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet, RouterLinkActive, RouterModule} from '@angular/router';
 
 @Component({
   selector: 'app-layout',
-  imports: [MatButtonModule, MatIcon, MatSidenavModule, RouterLink, RouterOutlet],
+  imports: [MatButtonModule, MatIcon, MatSidenavModule, RouterLink, RouterOutlet, RouterLinkActive, RouterModule],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
